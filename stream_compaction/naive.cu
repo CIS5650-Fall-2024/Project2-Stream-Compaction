399e2034bca1fc6fa732f1d8c9343421da331e69
Utkarsh Dwivedi <utkarshdwivedi3997@gmail.com>
2023-09-16 19:11:39 -0400

    naive gpu scan done

--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -12,14 +12,73 @@ namespace StreamCompaction {
             return timer;
         }
         // TODO: __global__
+        __global__ void kernelScan(int n, int* odata, const int* idata, int offset)
+        {
+            int index = (blockDim.x * blockIdx.x) + threadIdx.x;
+            if (index >= n)
+            {
+                return;     // invalid index
+            }
+
+            if (index >= offset)
+            {
+                odata[index] = idata[index - offset] + idata[index];
+            }
+            else
+            {
+                odata[index] = idata[index];
+            }
+        }
+
+        __global__ void kernelShiftRight(int n, int* odata, int* idata)
+        {
+            int index = (blockDim.x * blockIdx.x) + threadIdx.x;
+            if (index >= n)
+            {
+                return;     // invalid index
+            }
+
+            if (index == 0)
+            {
+                odata[index] = 0;
+            }
+            else
+            {
+                odata[index] = idata[index - 1];
+            }
+        }
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+            int* dev_idata;
+            int* dev_odata;
+            cudaMalloc((void**)&dev_idata, sizeof(int) * n);
+            checkCUDAError("cudaMalloc dev_idata failed");
+            cudaMalloc((void**)&dev_odata, sizeof(int) * n);
+            checkCUDAError("cudaMalloc dev_odata failed");
+            cudaMemcpy(dev_idata, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+            checkCUDAError("cudaMemcpy dev_idata failed");
+            cudaMemcpy(dev_odata, odata, sizeof(int) * n, cudaMemcpyHostToDevice);
+            checkCUDAError("cudaMemcpy dev_odata failed");
+
+            int totalBlocks = (n + blockSize - 1) / blockSize;
             timer().startGpuTimer();
-            // TODO
+            int max = ilog2ceil(n);
+            for (int d = 1; d <= max; d++)
+            {
+                kernelScan<<<totalBlocks, blockSize>>>(n, dev_odata, dev_idata, 1 << d - 1);
+                std::swap(dev_odata, dev_idata);
+            }
+            kernelShiftRight<<<totalBlocks, blockSize>>>(n, dev_odata, dev_idata);
             timer().endGpuTimer();
+            cudaMemcpy(odata, dev_odata, sizeof(int) * n, cudaMemcpyDeviceToHost);
+            checkCUDAError("cudaMemcpy dev_odata to odata failed");
+            cudaFree(dev_idata);
+            checkCUDAError("cudaFree dev_idata failed");
+            cudaFree(dev_odata);
+            checkCUDAError("cudaFree dev_odata failed");
         }
     }
 }
