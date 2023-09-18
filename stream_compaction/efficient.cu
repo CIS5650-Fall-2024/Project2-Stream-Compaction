89cc062f51c25b1e6448c345d0551972e4b8747c
Daniel <danielzhong2000@gmail.com>
2023-09-17 22:58:31 -0400

    Update efficient.cu

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -21,6 +21,10 @@ namespace StreamCompaction {
             
         }
 
+        __global__ void kernZero(const int n, int* data) {
+            data[n - 1] = 0;
+        }
+
         __global__ void kernDownSweep(int n, int* odata, int d) {
             int index = threadIdx.x + (blockIdx.x * blockDim.x);
             if (index >= n || (index % (int)powf(2, d + 1) != 0)) {
@@ -32,46 +36,81 @@ namespace StreamCompaction {
             odata[index + (int)powf(2, d + 1) - 1] += t;
         }
 
+        __global__ void kernCompact(int n, int* idata, int* odata) {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (index >= n) {
+                return;
+            }
+            
+            int k = 0;
+            if (idata[index] != 0) {
+                odata[index] = k;
+                k++;
+            }
+        }
+
+        __global__ void kernScatter(int n, int* odata) {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (index >= n) {
+                return;
+            }
+
+         
+
+        }
+
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
+        int nextPowerOf2(int n) {
+            if (n <= 0) return 0;
+
+            int power = 1;
+            while (power < n) {
+                power *= 2;
+            }
+
+            return power;
+        }
+
         void scan(int n, int *odata, const int *idata) {
             int* device_A;
 
-
-            cudaMalloc((void**)&device_A, n * sizeof(int));
+            int paddedSize = nextPowerOf2(n);
+            cudaMalloc((void**)&device_A, paddedSize * sizeof(int));
             checkCUDAError("cudaMalloc device_A failed!");
+
+
+            cudaMemset(device_A + n, 0, (paddedSize - n) * sizeof(int));
+            checkCUDAError("device_A cudaMemset failed!");
+
+            
 
             cudaMemcpy(device_A, idata, n * sizeof(int), cudaMemcpyHostToDevice);
             checkCUDAError("cudaMemcpy cudaMemcpyHostToDevice device_A to idata failed!");
 
-            dim3 blocksPerGrid((n + BlockSize - 1) / BlockSize);
+            dim3 blocksPerGrid((paddedSize + BlockSize - 1) / BlockSize);
 
             timer().startGpuTimer();
-            // TODO
-            for (int d = 0; d <= ilog2ceil(n) - 1; d++) { //Upsweep
-                kernUpSweep << <blocksPerGrid, BlockSize >> > (n, device_A, d);
-
+            
+            for (int d = 0; d <= ilog2ceil(paddedSize) - 1; d++) { //Upsweep
+                kernUpSweep << <blocksPerGrid, BlockSize >> > (paddedSize, device_A, d);
             }
 
-            // Set the root to zero before the downsweep
-            int zero = 0;
-            cudaMemcpy(&device_A[n - 1], &zero, sizeof(int), cudaMemcpyHostToDevice);
+            kernZero << <1, 1 >> > (paddedSize, device_A);
 
-            // Downsweep
-            for (int d = ilog2ceil(n) - 1; d >= 0; d--) {
-                kernDownSweep << <blocksPerGrid, BlockSize >> > (n, device_A, d);
+            for (int d = ilog2ceil(paddedSize) - 1; d >= 0; d--) { //Downsweep
+                kernDownSweep << <blocksPerGrid, BlockSize >> > (paddedSize, device_A, d);
             }
+
 
             timer().endGpuTimer();
 
+            
             cudaMemcpy(odata, device_A, n * sizeof(int), cudaMemcpyDeviceToHost);
-           
+            
 
 
-            for (int i = 0; i < n; i++) {
-                printf("%d ", odata[i]);
-            }
             checkCUDAError("cudaMemcpy cudaMemcpyDeviceToHost odata to device_A failed!");
 
             cudaFree(device_A);
@@ -88,9 +127,52 @@ namespace StreamCompaction {
          * @returns      The number of elements remaining after compaction.
          */
         int compact(int n, int *odata, const int *idata) {
+            int* device_A;
+            int* device_Binary;
+
+            int paddedSize = nextPowerOf2(n);
+            cudaMalloc((void**)&device_A, paddedSize * sizeof(int));
+            checkCUDAError("cudaMalloc device_A failed!");
+            cudaMalloc((void**)&device_Binary, paddedSize * sizeof(int));
+            checkCUDAError("cudaMalloc device_Binary failed!");
+
+            cudaMemcpy(device_A, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+            checkCUDAError("cudaMemcpy cudaMemcpyHostToDevice device_A to idata failed!");
+
+
+            cudaMemset(device_A + n, 0, (paddedSize - n) * sizeof(int));
+            checkCUDAError("device_A cudaMemset failed!");
+            cudaMemset(device_Binary + n, 0, (paddedSize - n) * sizeof(int));
+            checkCUDAError("device_Binary cudaMemset failed!");
+
+            dim3 blocksPerGrid((paddedSize + BlockSize - 1) / BlockSize);
+
             timer().startGpuTimer();
-            // TODO
+
+            for (int d = 0; d <= ilog2ceil(paddedSize) - 1; d++) { //Upsweep
+                kernUpSweep << <blocksPerGrid, BlockSize >> > (paddedSize, device_A, d);
+            }
+
+            kernZero << <1, 1 >> > (paddedSize, device_A);
+
+            for (int d = ilog2ceil(paddedSize) - 1; d >= 0; d--) { //Downsweep
+                kernDownSweep << <blocksPerGrid, BlockSize >> > (paddedSize, device_A, d);
+            }
+            kernCompact << <blocksPerGrid, BlockSize >> > (paddedSize, device_A, device_Binary);
+
             timer().endGpuTimer();
+
+            cudaMemcpy(odata, device_Binary, n * sizeof(int), cudaMemcpyDeviceToHost);
+
+            for (int i = 0; i < n; i++) {
+                printf("%d ", odata[i]);
+            }
+
+
+            checkCUDAError("cudaMemcpy cudaMemcpyDeviceToHost odata to device_A failed!");
+
+            cudaFree(device_A);
+            checkCUDAError("cudaFree device_A failed!");
             return -1;
         }
     }
