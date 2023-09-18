bd4da5d9437a027c7e02d2269326e4359a597659
Jason Li <jasonsli@seas.upenn.edu>
2023-09-18 02:07:23 -0400

    Parts 1 and 2 finished

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -12,6 +12,15 @@ namespace StreamCompaction {
             return timer;
         }
 
+        void scan_impl(int n, int* odata, const int* idata)
+        {
+            odata[0] = 0;
+            for (int i = 1; i < n; i++)
+            {
+                odata[i] = idata[i - 1] + odata[i - 1];
+            }
+        }
+
         /**
          * CPU scan (prefix sum).
          * For performance analysis, this is supposed to be a simple for loop.
@@ -19,7 +28,7 @@ namespace StreamCompaction {
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            scan_impl(n, odata, idata);
             timer().endCpuTimer();
         }
 
@@ -30,9 +39,19 @@ namespace StreamCompaction {
          */
         int compactWithoutScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            int output_index = 0;
+            int curr_data;
+            for (int i = 0; i < n; i++)
+            {
+                curr_data = idata[i];
+                if (curr_data != 0)
+                {
+                    odata[output_index] = curr_data;
+                    output_index++;
+                }
+            }
             timer().endCpuTimer();
-            return -1;
+            return output_index;
         }
 
         /**
@@ -42,9 +61,25 @@ namespace StreamCompaction {
          */
         int compactWithScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            int *temp = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                temp[i] = idata[i] != 0 ? 1 : 0;
+            }
+            int* scan_result = new int[n];
+            scan_impl(n, scan_result, temp);
+
+            int final_index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (temp[i] == 1)
+                {
+                    final_index = scan_result[i];
+					odata[final_index] = idata[i];
+				}
+            }
             timer().endCpuTimer();
-            return -1;
+            return final_index + 1;
         }
     }
 }
--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -3,6 +3,8 @@
 #include "common.h"
 #include "naive.h"
 
+#define block_size 128
+
 namespace StreamCompaction {
     namespace Naive {
         using StreamCompaction::Common::PerformanceTimer;
@@ -12,14 +14,97 @@ namespace StreamCompaction {
             return timer;
         }
         // TODO: __global__
+        __global__ void kern_scan_global(int d, int n, const int* idata, int* odata)
+        {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (index >= n)
+            {
+				return;
+			}
+            int offset = (1 << (d - 1));
+            if (index >= offset)
+            {
+                odata[index] = idata[index - offset] + idata[index];
+            }
+            else
+            {
+				odata[index] = idata[index];
+			}
+        }
+
+        __global__ void kern_inclusive_to_exclusive(int n, const int* idata, int* odata)
+        {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+			if (index >= n)
+			{ 
+                return;
+            }
+            if (index == 0)
+            {
+				odata[index] = 0;
+			}
+            else
+            {
+				odata[index] = idata[index - 1];
+			}
+        }
+
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+            int *dev_idata;
+            int* dev_odata;
+            dim3 full_blocks_per_grid((n + block_size - 1) / block_size);
+
+            cudaMalloc((void**)&dev_idata, n * sizeof(int));
+            checkCUDAErrorFn("cudaMalloc dev_idata failed!");
+            cudaMalloc((void**)&dev_odata, n * sizeof(int));
+            checkCUDAErrorFn("cudaMalloc dev_odata failed!");
+
+            cudaMemcpy(dev_idata, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+            checkCUDAErrorFn("cudaMemcpy dev_idata failed!");
+
             timer().startGpuTimer();
-            // TODO
+            
+            for (int i = 0; i < ilog2ceil(n); i++)
+            {
+                kern_scan_global <<<full_blocks_per_grid, block_size>>> (i + 1, n, dev_idata, dev_odata);
+				std::swap(dev_idata, dev_odata);
+            }
+            kern_inclusive_to_exclusive <<<full_blocks_per_grid, block_size>>> (n, dev_idata, dev_odata);
+
             timer().endGpuTimer();
+            cudaMemcpy(odata, dev_odata, sizeof(int) * n, cudaMemcpyDeviceToHost);
+            checkCUDAErrorFn("cudaMemcpy dev_odata failed!");
+
+            cudaFree(dev_idata);
+            checkCUDAErrorFn("cudaFree dev_idata failed!");
+            cudaFree(dev_odata);
+            checkCUDAErrorFn("cudaFree dev_odata failed!");
+
         }
+
+
+
+        //__global__ void scan(float* g_odata, float* g_idata, int n) {
+        //    extern __shared__ float temp[]; // allocated on invocation    
+        //    int thid = threadIdx.x;
+        //    int pout = 0, pin = 1;   // Load input into shared memory.    // This is exclusive scan, so shift right by one    // and set first element to 0   
+        //    temp[pout * n + thid] = (thid > 0) ? g_idata[thid - 1] : 0;
+        //    __syncthreads();
+        //    for (int offset = 1; offset < n; offset *= 2)
+        //    {
+        //        pout = 1 - pout; // swap double buffer indices     
+        //        pin = 1 - pout;
+        //        if (thid >= offset)
+        //            temp[pout * n + thid] += temp[pin * n + thid - offset];
+        //        else
+        //            temp[pout * n + thid] = temp[pin * n + thid];
+        //        __syncthreads();
+        //    }
+        //    g_odata[thid] = temp[pout * n + thid]; // write output 
+        //}
     }
 }
