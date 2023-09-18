cd15904870d24e7432973ccf8ff7800cade4f2e8
Utkarsh Dwivedi <utkarshdwivedi3997@gmail.com>
2023-09-17 23:12:42 -0400

    efficient scan works for non power of 2, can probably improve it?

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -96,10 +96,74 @@ namespace StreamCompaction {
          * @returns      The number of elements remaining after compaction.
          */
         int compact(int n, int *odata, const int *idata) {
+            int* dev_idata;
+            cudaMalloc((void**)&dev_idata, sizeof(int) * n);
+            checkCUDAError("cudaMalloc dev_idata failed");
+            cudaMemcpy(dev_idata, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+            checkCUDAError("cudaMemcpy dev_idata failed");
+
+            int* dev_odata;
+            cudaMalloc((void**)&dev_odata, sizeof(int) * n);
+            checkCUDAError("cudaMalloc dev_idata failed");
+
+            int* dev_bools;
+            cudaMalloc((void**)&dev_bools, sizeof(int) * n);
+            checkCUDAError("cudaMalloc dev_bools failed");
+            cudaMemset(dev_bools, -1, sizeof(int) * n);
+            checkCUDAError("cudaMemset dev_bools failed");
+
+            int totalBlocks = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+            int max = ilog2ceil(n);
+            int nNextPowerOf2 = pow(2, max);
+            int totalBlocksPow2 = (nNextPowerOf2 + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+
+            int* dev_indices;
+            cudaMalloc((void**)&dev_indices, sizeof(int) * nNextPowerOf2);
+            checkCUDAError("cudaMalloc dev_indices failed");
+            cudaMemset(dev_indices, -1, sizeof(int) * nNextPowerOf2);       // -1
+            checkCUDAError("cudaMemset dev_indices failed");
+
             timer().startGpuTimer();
-            // TODO
+            // Map to bools array
+            StreamCompaction::Common::kernMapToBoolean<<<totalBlocks, BLOCK_SIZE>>>(n, dev_bools, dev_idata);
+            
+            // Scan step
+            // Copy bools to indices array to run scan on
+            cudaMemcpy(dev_indices, dev_bools, sizeof(int) * n, cudaMemcpyDeviceToDevice);
+            // Upsweep
+            for (int d = 0; d < max; d++)
+            {
+                kernelUpSweep<<<totalBlocksPow2, BLOCK_SIZE>>> (nNextPowerOf2, dev_indices, d);
+            }
+
+            cudaMemset(dev_indices + nNextPowerOf2 - 1, 0, sizeof(int));
+            checkCUDAError("cudaMemset dev_indices failed");
+
+            // downsweep
+            for (int d = max - 1; d >= 0; d--)
+            {
+                kernelDownSweep<<<totalBlocksPow2, BLOCK_SIZE>>>(nNextPowerOf2, dev_indices, d);
+            }
+
+            // Scatter step
+            StreamCompaction::Common::kernScatter<<<totalBlocks, BLOCK_SIZE>>>(n, dev_odata, dev_idata, dev_bools, dev_indices);
             timer().endGpuTimer();
-            return -1;
+
+            int len;
+            // I have no idea why I need this ternary check below
+            cudaMemcpy(&len, dev_indices + n + (n == nNextPowerOf2 ? -1 : 0), sizeof(int), cudaMemcpyDeviceToHost);  // length of compacted array
+            checkCUDAError("cudaMemcpy dev_indices last element failed");
+            cudaMemcpy(odata, dev_odata, sizeof(int) * len, cudaMemcpyDeviceToHost);
+            checkCUDAError("cudaMemcpy dev_odata failed");
+
+            cudaFree(dev_idata);
+            cudaFree(dev_odata);
+            cudaFree(dev_bools);
+            cudaFree(dev_indices);
+
+            return len;
         }
     }
 }
