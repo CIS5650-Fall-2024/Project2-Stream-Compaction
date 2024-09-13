83c3f75a8c1331d60ee510d354b71499810c77d2
Runjie Zhao <runjiezhao8@gmail.com>
2024-09-13 13:22:22 -0700

    cpu, naive, and scan efficient done

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -1,5 +1,6 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <iostream>
 #include "common.h"
 #include "efficient.h"
 
@@ -12,13 +13,89 @@ namespace StreamCompaction {
             return timer;
         }
 
+        //variable initialization
+        int* dev_idata;
+        int* dev_origin;
+        const int blockSize = 64;
+
+        //GPU scan
+        __global__ void upSweep(int n, int logVal, int* idata) {
+            int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+            if (index >= n)return;
+
+            int offset1 = 1 << (logVal + 1);
+            int offset2 = 1 << logVal;
+            if (index % offset1 == 0) {
+                idata[index + offset1 - 1] += idata[index + offset2 - 1];
+            }
+        }
+
+        __global__ void downSweep(int n, int logVal, int* idata) {
+            int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+            if (index >= n)return;
+
+            int offset1 = 1 << (logVal + 1);
+            int offset2 = 1 << logVal;
+
+            if (index % offset1 == 0) {
+                int t = idata[index + offset1 - 1];
+                idata[index + offset1 - 1] += idata[index + offset2 - 1];
+                idata[index + offset2 - 1] = t;
+            }
+        }
+
+        __global__ void shiftArray(int n, int* origin, int* idata) {
+            int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+            if (index >= n)return;
+
+            idata[index] += origin[index];
+        }
+
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+            int size = pow(2, ilog2ceil(n));
+
+            cudaMalloc((void**)&dev_idata, size * sizeof(int));
+            checkCUDAError("cudaMalloc dev_idata failed!");
+            cudaMemset(dev_idata, 0, size * sizeof(int));
+            checkCUDAError("cudaMemset dev_idata failed!");
+            cudaMemcpy(dev_idata, idata, size * sizeof(int), cudaMemcpyHostToDevice);
+            checkCUDAError("cudaMemcpy dev_idata failed!");
+
+            cudaMalloc((void**)&dev_origin, size * sizeof(int));
+            checkCUDAError("cudaMalloc dev_idata failed!");
+            cudaMemset(dev_origin, 0, size * sizeof(int));
+            checkCUDAError("cudaMemset dev_idata failed!");
+            cudaMemcpy(dev_origin, idata, size * sizeof(int), cudaMemcpyHostToDevice);
+            checkCUDAError("cudaMemcpy dev_idata failed!");
+
+            dim3 fullBlocksPerGrid((n + blockSize - 1) / blockSize);
             timer().startGpuTimer();
             // TODO
+            for (int i = 0; i <= ilog2ceil(n) - 1; i++) {
+                upSweep << <fullBlocksPerGrid, blockSize >> > (size, i, dev_idata);
+                checkCUDAError("cudaFunc upSweep failed!");
+            }
+
+            cudaMemset(dev_idata + size - 1, 0, sizeof(int));
+            checkCUDAError("cudaMemset dev_idata + size - 1 failed!");
+
+            for (int i = ilog2ceil(n) - 1; i >= 0; i--) {
+                downSweep << <fullBlocksPerGrid, blockSize >> > (size, i, dev_idata);
+                checkCUDAError("cudaFunc downSweep failed!");
+            }
+
+            shiftArray << <fullBlocksPerGrid, blockSize >> > (size, dev_origin, dev_idata);
+
             timer().endGpuTimer();
+            cudaMemcpy(odata, dev_idata, sizeof(int) * n, cudaMemcpyDeviceToHost);
+            cudaFree(dev_idata);
+            cudaFree(dev_origin);
         }
 
         /**
@@ -33,6 +110,7 @@ namespace StreamCompaction {
         int compact(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
             // TODO
+            
             timer().endGpuTimer();
             return -1;
         }
