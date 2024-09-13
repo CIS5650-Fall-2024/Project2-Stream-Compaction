b6d35e89915d1ca89c8b9b7d00b3ef0f9b7fc1d6
Matt Schwartz <GITHUB_USER@users.noreply.github.com>
2024-09-13 17:53:22 -0400

    Finishes implementation of efficient scan of arbitrary length

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -15,43 +15,35 @@ namespace StreamCompaction {
 
         const int MAX_BLOCK_SIZE = 1024;
 
-        /**
-         * Performs prefix-sum (aka scan) on idata, storing the result into odata.
-         */
-        void scan(int n, int *odata, const int *idata) {
-            int n_padded = pow(2, ilog2ceil(n)); // pad array to nearest power of two
+        void scan(int n_padded, int* dev_data) {
+            int maxBlockSize = std::min((n_padded / 2), MAX_BLOCK_SIZE);
+            int stride = 2 * maxBlockSize; // since each thread works on two entries of dev_data, 2x block size gives the stride to get between blocks of data in dev_data.
+            dim3 blocksPerGrid = ((n_padded / 2) + maxBlockSize - 1) / maxBlockSize;
 
-            int* dev_data;
-            cudaMalloc((void**) &dev_data, n_padded * sizeof(int));
-            cudaMemcpy(dev_data, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+            int* stored_sums; // temp array used to last entry per block during upsweep. See kernZeroEntries and kernIncrement for use info.
+            cudaMalloc((void**)&stored_sums, blocksPerGrid.x * sizeof(int));
 
-            // Pad input data to be a power of two, if needed.
-            if (n < n_padded) {
-                cudaMemset(dev_data + n, 0, (n_padded - n) * sizeof(int));
-            }
+            int blockSize_i = maxBlockSize;
+            for (int depth = 0; depth < ilog2ceil(2 * maxBlockSize); ++depth) {
+                kernUpSweep<<<blocksPerGrid, blockSize_i>>>(n_padded, stride, depth, dev_data);
 
-            int blockSize = std::min((n_padded / 2), MAX_BLOCK_SIZE);
-            dim3 blocksPerGrid = ((n_padded / 2) + blockSize - 1) / blockSize;
-
-            int* stored_sums; // temp array used to last entry per block during upsweep. See kernBlockSums for more info.
-            cudaMalloc((void**) &stored_sums, blocksPerGrid.x * sizeof(int));
-
-            // timer().startGpuTimer();
-            
-            for (int depth = 0; depth < ilog2ceil(2 * blockSize); ++depth) {
-                kernUpSweep<<<blocksPerGrid, blockSize>>>(n_padded, depth, dev_data);
-
-                blockSize /= 2; // need fewer threads each iteration
+                blockSize_i /= 2; // need fewer threads each iteration
                 cudaDeviceSynchronize();
             }
 
+            // Between upsweep and downsweep, zero out the last entry of every block (first storing off those entries for later use)
+            int zeroEntriesBlockSize = 128;
+            dim3 zeroEntriesBlocksPerGrid = (blocksPerGrid.x + zeroEntriesBlockSize - 1) / zeroEntriesBlockSize;
+            kernZeroEntries<<<zeroEntriesBlocksPerGrid, zeroEntriesBlockSize>>> (blocksPerGrid.x, stride, dev_data, stored_sums);
+            cudaDeviceSynchronize();
+
             // Keep blocks per grid constant, so we can handle arbitrarily sized arrays.
             // But grow blockSize on each iteration since we need more threads on each depth layer.
-            blockSize = 1;
-            for (int depth = ilog2ceil(2 * blockSize) - 1; depth >= 0; --depth) {
-                kernDownSweep<<<blocksPerGrid, blockSize>>>(n_padded, depth, dev_data, stored_sums);
+            blockSize_i = 1;
+            for (int depth = ilog2ceil(2 * maxBlockSize) - 1; depth >= 0; --depth) {
+                kernDownSweep<<<blocksPerGrid, blockSize_i>>>(n_padded, stride, depth, dev_data);
 
-                blockSize *= 2; // need more threads each iteration
+                blockSize_i *= 2; // need more threads each iteration
                 cudaDeviceSynchronize();
             }
 
@@ -60,85 +52,82 @@ namespace StreamCompaction {
             //
             // This needs to be done recursively to handle arbitrarily large arrays.
             if (n_padded > 2 * MAX_BLOCK_SIZE) {
-                // TODO - partial scans are all exclusive... need to be inclusive for this step
-
-                // Scatter the sums from the previous scan operation
-                int* sum_data;
-                cudaMalloc((void**) &sum_data, blocksPerGrid.x * sizeof(int));
-                int sumBlockSize = 128;
-                dim3 sumBlocksPerGrid = (blocksPerGrid.x + sumBlockSize - 1) / sumBlockSize;
-                kernBlockSums<<<sumBlocksPerGrid, sumBlockSize>>>(blocksPerGrid.x, blockSize, dev_data, sum_data, stored_sums);
-                cudaDeviceSynchronize();
-
                 // (Recursively) scan the summed blocks array
                 // Can use sum_data as both the input and output pointers for the scan. No issue writing over it.
-                scan(blocksPerGrid.x, sum_data, sum_data);
+                scan(blocksPerGrid.x, stored_sums);
 
                 // Finally, add scanned sum values back to the original dev_data
                 // In original scan, each thread handled 2 elements. In this step, each handles one, so we need 2x the blocks.
-                dim3 kernBlocksPerGrid = 2 * blocksPerGrid.x; 
-                kernIncrement<<<kernBlocksPerGrid, blockSize>>>(n_padded, dev_data, sum_data);
+                dim3 kernBlocksPerGrid = 2 * blocksPerGrid.x;
+                kernIncrement<<<kernBlocksPerGrid, maxBlockSize>>> (n_padded, dev_data, stored_sums);
                 cudaDeviceSynchronize();
-
-                cudaFree(sum_data);
             }
 
-            // TODO - the recursive scan  is going to trigger multiple timer calls... create wrapper function for timing.
-            // Actually I think best bet is to modify timer to not restart if it's already been started? But then what about end... wrapper function for that?
-            // timer().endGpuTimer();
             cudaFree(stored_sums);
+        }
+
+        /**
+         * Wrapper around scan (to facilitate gpu timing and allocating things)
+         */
+        void scan(int n, int *odata, const int *idata) {
+            int n_padded = pow(2, ilog2ceil(n)); // pad array to nearest power of two
+
+            int* dev_data;
+            cudaMalloc((void**)&dev_data, n_padded * sizeof(int));
+            cudaMemcpy(dev_data, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+
+            // Pad input data to be a power of two, if needed.
+            if (n < n_padded) {
+                cudaMemset(dev_data + n, 0, (n_padded - n) * sizeof(int));
+            }
+
+            timer().startGpuTimer();
+            scan(n_padded, dev_data);
+            timer().endGpuTimer();
+
             cudaMemcpy(odata, dev_data, n_padded * sizeof(int), cudaMemcpyDeviceToHost);
             cudaFree(dev_data);
         }
 
-        __global__ void kernUpSweep(int n, int depth, int* dev_data) {
+        __global__ void kernUpSweep(int n, int stride, int depth, int* dev_data) {
             int threadId = threadIdx.x; 
-            if (threadId + (blockDim.x * blockIdx.x) >= n/2) return;
 
             int twoToDepthPlusOne = (1 << (depth + 1));
             int twoToDepth = (1 << depth);
             // Since each block is a self contained scan, we calculate these indices w.r.t the local block thread index.
             // But then we offset by (blockDim.x * blockIdx.x) because the dev_data is for ALL blocks, so we need to access the right part.
-            int leftChildIdx = (threadId * twoToDepthPlusOne) + twoToDepth - 1 + (blockDim.x * blockIdx.x);
-            int rightChildIdx = (threadId * twoToDepthPlusOne) + twoToDepthPlusOne - 1 + (blockDim.x * blockIdx.x);
+            int leftChildIdx = (threadId * twoToDepthPlusOne) + twoToDepth - 1 + (stride * blockIdx.x);
+            int rightChildIdx = (threadId * twoToDepthPlusOne) + twoToDepthPlusOne - 1 + (stride * blockIdx.x);
+
+            if (rightChildIdx >= n) return;
 
             dev_data[rightChildIdx] += dev_data[leftChildIdx];
         }
 
-        __global__ void kernDownSweep(int n, int depth, int* dev_data, int* stored_sums) {
+        __global__ void kernZeroEntries(int n, int stride, int* dev_data, int* stored_sums) {
+            int threadId = threadIdx.x + (blockDim.x * blockIdx.x);
+            if (threadId >= n) return;
+
+            int dev_data_idx = (threadId + 1) * stride - 1;
+            stored_sums[threadId] = dev_data[dev_data_idx];
+            dev_data[dev_data_idx] = 0;
+        }
+
+        __global__ void kernDownSweep(int n, int stride, int depth, int* dev_data) {
             int threadId = threadIdx.x; 
-            if (threadId + (blockDim.x * blockIdx.x) >= n/2) return;
 
             int twoToDepthPlusOne = (1 << (depth + 1));
             int twoToDepth = (1 << depth);
             int blockLeftChildIdx = (threadId * twoToDepthPlusOne) + twoToDepth - 1;
-            int globalLeftChildIdx = blockLeftChildIdx + (blockDim.x * blockIdx.x);
+            int globalLeftChildIdx = blockLeftChildIdx + (stride * blockIdx.x);
             int blockRightChildIdx = (threadId * twoToDepthPlusOne) + twoToDepthPlusOne - 1;
-            int globalRightChildIdx = blockRightChildIdx + + (blockDim.x * blockIdx.x);
+            int globalRightChildIdx = blockRightChildIdx + (stride * blockIdx.x);
 
-            if (blockRightChildIdx == (blockDim.x - 1)) {
-                stored_sums[blockIdx.x] = dev_data[blockRightChildIdx]; // need this for later kernel
-                dev_data[blockRightChildIdx] = 0; // zero out last element in block
-            }
+            if (globalRightChildIdx >= n) return;
 
             int leftVal = dev_data[globalLeftChildIdx];
             dev_data[globalLeftChildIdx] = dev_data[globalRightChildIdx];
-            dev_data[globalLeftChildIdx] += leftVal;
-        }
-
-        /**
-         * Kernel to scatter sums from individual blocks into a sum_data array.
-         * In most use cases, the sum_data array is probably small and this would be faster on the CPU, but for REALLY big input arrays,
-         * it might be worth it to do this step on the GPU.
-         * 
-         * Here, n is the number of blocks used in the upsweep / downsweep steps above.
-         */
-        __global__ void kernBlockSums(int n, int stride, const int* dev_data, int* sum_data, const int* stored_sums) {
-            int threadId = threadIdx.x + (blockDim.x * blockIdx.x);
-            if (threadId > n) return;
-
-            // Note: since the partial scans on dev_data were exclusive, we need to do one more addition to get the inclusive scan amount.
-            sum_data[threadId] = dev_data[(threadId * stride) + (stride - 1)] + stored_sums[threadId]; 
+            dev_data[globalRightChildIdx] += leftVal;
         }
 
         /**
@@ -148,10 +137,12 @@ namespace StreamCompaction {
          */
         __global__ void kernIncrement(int n, int* dev_data, int* sum_data) {
             int threadId = threadIdx.x + (blockIdx.x * blockDim.x);
-            if (threadId > n) return;
+            if (threadId >= n) return;
 
-            // Divide blockIdx.x by 2 because we're using 2x the blocks for this step compared to the original scan.
-            dev_data[threadId] += sum_data[blockIdx.x / 2];
+            // The extra factor of 2 comes from the fact that we're using twice as many block here as in the original scan.
+            int sum_data_idx = gridDim.x * threadId / (2 * n); // note: integer division here
+
+            dev_data[threadId] += sum_data[sum_data_idx];
         }
 
         /**
