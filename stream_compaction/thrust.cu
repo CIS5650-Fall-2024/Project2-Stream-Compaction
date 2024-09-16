f3a09d58b05d6c5d70811840d05ee7600c98398f
Nicholas Liu <liunicholas6@gmail.com>
2024-09-16 16:23:41 -0400

    thrust impl

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -3,11 +3,24 @@
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
 #include <thrust/scan.h>
+#include <thrust/sort.h>
 #include "common.h"
 #include "thrust.h"
 
 namespace StreamCompaction {
     namespace Thrust {
+        struct TimerGuard
+        {
+            TimerGuard()
+            {
+                timer().startGpuTimer();
+            }
+            ~TimerGuard()
+            {
+                timer().endGpuTimer();
+            }
+        };
+
         using StreamCompaction::Common::PerformanceTimer;
         PerformanceTimer& timer()
         {
@@ -18,11 +31,21 @@ namespace StreamCompaction {
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
-            timer().startGpuTimer();
-            // TODO use `thrust::exclusive_scan`
-            // example: for device_vectors dv_in and dv_out:
-            // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
-            timer().endGpuTimer();
+            size_t numBytes = n * sizeof(int);
+
+            int *dev_idata;
+            cudaMalloc(&dev_idata, numBytes);
+            cudaMemcpy(dev_idata, idata, numBytes, cudaMemcpyHostToDevice);
+
+            int *dev_odata;
+            cudaMalloc(&dev_odata, numBytes);
+
+            {
+                TimerGuard _;
+                thrust::exclusive_scan(thrust::device, dev_idata, dev_idata + n, dev_odata);
+            }
+
+            cudaMemcpy(odata, dev_odata, numBytes, cudaMemcpyDeviceToHost);
         }
     }
 }
