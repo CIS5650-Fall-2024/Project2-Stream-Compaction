8cb7f428d5e498873bab4d1a01c5a78d32c4a4f8
Adithya Rajeev <adithyar262@gmail.com>
2024-09-17 18:53:29 -0400

    Implemented common helper functions
    Implemented kernScatter and kernMapToBoolean functions

--- stream_compaction/common.cu
+++ stream_compaction/common.cu
@@ -24,6 +24,10 @@ namespace StreamCompaction {
          */
         __global__ void kernMapToBoolean(int n, int *bools, const int *idata) {
             // TODO
+            int idx = blockIdx.x * blockDim.x + threadIdx.x;
+            if (idx < n) {
+                bools[idx] = (idata[idx] != 0);
+            }
         }
 
         /**
@@ -33,6 +37,10 @@ namespace StreamCompaction {
         __global__ void kernScatter(int n, int *odata,
                 const int *idata, const int *bools, const int *indices) {
             // TODO
+            int idx = blockIdx.x * blockDim.x + threadIdx.x;
+            if (idx < n && bools[idx]) {
+                odata[indices[idx]] = idata[idx];
+            }
         }
 
     }
